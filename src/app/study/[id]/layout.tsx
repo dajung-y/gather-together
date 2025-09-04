22f425a907e620ad7356cd1a91537fcce748754c
6108 <73987879+6108@users.noreply.github.com>
2025-09-04 16:48:57 +0900

    feat: 사이드바 컴포넌트 구현 #6 (#11)
    Co-authored-by: 6108 <>

--- src/app/study/[id]/layout.tsx
+++ src/app/study/[id]/layout.tsx
@@ -1,7 +1,33 @@
+import Sidebar, { handleLeaveRoom } from "@/components/common/Sidebar";
+import { MenuItem } from "@/types/sidebar";
+
+const menuItems: MenuItem[] = [
+  //링크 이동
+  {
+    label: '내가 지원한 스터디',
+    path: '/study/1'
+  },
+  {
+    label: '내가 만든 스터디',
+    path: '/study/1/setting'
+  },
+
+  //함수 실행
+  //useClient 컴포넌트에서만 함수를 만들 수 있기 때문에
+  //sidebar component에서 함수 만든 후 export해서 사용
+  {
+    label: '탈퇴하기',
+    onClick: handleLeaveRoom
+  },
+]
+
 export default function Layout({ children }: { children: React.ReactNode; }) {
   return (
     <div>
-      <div>Layout</div>
+      <div className="fixed top-20 left-0">
+        {/* 아래 수정필요! */}
+        <Sidebar title="훈정님의 페이지" menuItems={menuItems} />
+      </div>
       <div>{children}</div>
     </div>
   );
