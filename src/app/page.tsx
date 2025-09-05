e1d9bc643662a597d7a9c7d82e9d3a6cd9f6debe
dajung-y <0352wjd@naver.com>
2025-09-05 13:33:07 +0900

    feat: 메인페이지 캐러셀 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
-'use client'
-import AlertModal from "@/components/common/AlertModal";
-import { useState } from "react";
+import Carousel from "@/components/main/Carousel";
 
 export default function Home() {
-  const [open, setOpen] = useState(false);
-  return (
-    <>
-      <button
-        onClick={() => setOpen(true)}
-        className="px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Alert 모달 열기
-      </button>
 
-      <AlertModal
-        isOpen={open}
-        onClose={() => setOpen(false)}
-        title="스터디룸이 생성되었습니다"
-        subtitle="스터디 룸으로 이동할까요?"
-        onConfirm={() => {
-          alert("확인 처리 로직 실행!");
-        }}
-        confirmText="확인"
-        cancelText="취소"
-      />
-    </>
+  return (
+    <main className="w-full min-h-screen flex justify-center">
+      <div className="w-full max-w-[1280px] px-4">
+        <section className="py-4">
+          <Carousel />
+        </section>
+      </div>
+    </main>
   );
 }
