b6ce12f5414c8267245da48d48b1dcd07eaf384a
dajung-y <0352wjd@naver.com>
2025-09-05 13:40:19 +0900

    chore: 이미지파일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,28 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen flex justify-center">
       <div className="w-full max-w-[1280px] px-4">
+        {/* 캐러셀 */}
         <section className="py-4">
           <Carousel />
+        </section>
+        {/* 중앙 컨트롤 영역 */}
+        <section>
+          {/* 모집중 버튼, 검색창 */}
+          <div>
+
+          </div>
+          {/* 필터 */}
+          <div>
+
+          </div>
+        </section>
+        {/* 카드 리스트 */}
+        <section>
+
+        </section>
+        {/* 페이지네이션 */}
+        <section>
+          
         </section>
       </div>
     </main>
