09097dcb06b01fe233ccd0957cdf37116dd7fad7
6108 <>
2025-09-16 17:36:16 +0900

    feat: 공지 DELETE api

--- src/app/api/notice/route.ts
+++ src/app/api/notice/route.ts
@@ -1,24 +1,25 @@
 import clientPromise from "@/lib/mongodb";
 import { Notice } from "@/types/notice";
+import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { groupId, title, content } = body;
+    const { studyId, title, content } = body;
 
     const client = await clientPromise;
     const db = client.db();
 
-    const newNotice: Notice = {
-      groupId,
+    const noticeData = {
+      studyId,
       title,
       content,
       createdAt: new Date(),
       updatedAt: new Date(),
     };
 
-    const result = await db.collection("notices").insertOne(newNotice);
+    const result = await db.collection("notices").insertOne(noticeData);
 
     return NextResponse.json(
       { success: true, taskId: result.insertedId },
@@ -33,15 +34,25 @@ export async function POST(req: Request) {
 }
 
 export async function GET(req: Request) {
-  const groupId = new URL(req.url).searchParams.get("groupId");
-  if (!groupId) return NextResponse.json({ error: "groupId 필요" }, { status: 400 });
+  const studyId = new URL(req.url).searchParams.get("studyId");
+  if (!studyId) return NextResponse.json({ error: "studyId 필요" }, { status: 400 });
 
   const db = (await clientPromise).db();
   const notices = await db
     .collection("notices")
-    .find({ groupId })
+    .find({ studyId })
     .sort({ createdAt: -1 })
     .toArray();
 
   return NextResponse.json({ success: true, data: notices.map(n => ({ ...n, _id: n._id.toString() })) });
+}
+
+export async function DELETE(req: Request) {
+  const { id } = (await req.json()) as { id: string }; // 문자열로 명시
+  if (!id) return NextResponse.json({ error: "id 필요" }, { status: 400 });
+
+  const db = (await clientPromise).db();
+  const result = await db.collection("notices").deleteOne({ _id: new ObjectId(id) });
+
+  return NextResponse.json({ success: result.deletedCount === 1 });
 }
