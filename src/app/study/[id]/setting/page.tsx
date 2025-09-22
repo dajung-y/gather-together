bf3e11537589f36531ebee75ce08cbbfc8697dd8
6108 <>
2025-09-22 09:14:29 +0900

    feat: 스터디 스케쥴 변경 api

--- src/app/study/[id]/setting/page.tsx
+++ src/app/study/[id]/setting/page.tsx
@@ -1,5 +1,7 @@
+import WeekdaySelect from "@/components/study/create/WeekdaySelect";
 import MemberList from "@/components/study/main/MemberList";
 import UserDelete from "@/components/study/main/UserDelete";
+import StudySchedule from "@/components/study/setting/StudySchedule";
 import clientPromise from "@/lib/mongodb";
 import { getUserIdFromSession } from "@/lib/session";
 import { getStudyData } from "@/lib/study";
@@ -20,7 +22,16 @@ export default async function page({ params }: { params: { id: string } }) {
     redirect('/');
   }
 
-  const studyData: StudyData = await getStudyData(studyId);
+  // const studyData: StudyData = await getStudyData(studyId);
+  const client = await clientPromise;
+  const db = client.db();
+
+  const study = await db.collection("studies").findOne({
+    _id: new ObjectId(studyId)
+  });
+
+  const studyData: StudyData = JSON.parse(JSON.stringify(study));
+
 
   const isLeader = studyData.members.some(
     (m) => m.userId === userId && m.role === "leader");
@@ -39,30 +50,17 @@ export default async function page({ params }: { params: { id: string } }) {
             <p className="headline4 text-primary-500 my-4">유저</p>
             <div className="flex gap-2">
               <MemberList studyId={studyId} memberData={studyData.members} />
-              {/* {studyData.members?.map((member, index) => (
-            <UserDelete name={member.nickname} key={index} />
-          ))}
-          <UserDelete name="김훈정" />
-          <UserDelete name="문지현" />
-          <UserDelete name="윤다정" /> */}
             </div>
             <div className="h-12"></div>
             {/* 스터디 시간 */}
             <p className="headline4 text-primary-500 my-4">스터디 시간</p>
             <div className="flex gap-2 items-center">
-              <span>요일</span>
               <div className="flex gap-2 mt-2">
-
-
+                <StudySchedule studyId={studyId} st={studyData.startTime}
+                  et={studyData.endTime} wd={studyData.weekdays} />
               </div>
             </div>
-
-            <div className="h-12"></div>
           </div>}
-
-
-        <p className="headline4 text-primary-500 my-4">스터디 탈퇴하기</p>
-
       </div>
     </>
   )
